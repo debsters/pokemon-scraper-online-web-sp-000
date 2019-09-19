12dce77fc80f0e60368935b1524b4bb5a4207c6b
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-09-19 18:16:08 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,6 +19,8 @@ class Pokemon
       Pokemon.new(id: row[0], name: row[1], type: row[2], db: db)
   end
 
+end
+
   # def self.save(name, type, db)
   #   sql = <<-SQL
   #   INSERT INTO pokemon (name, type)
