48ef0962a36dc4f26396c68739735eb9f0e73a11
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-09-19 18:21:16 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,16 +10,34 @@ class Pokemon
   end
 
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?);", name, type)
+    sql = <<-SQL
+    INSERT INTO pokemon (name, type)
+    VALUES (?, ?)
+    SQL
+  
+    db.execute(sql, name, type)
     @id = db.execute('SELECT last_insert_rowid() FROM pokemon')[0][0]
   end
 
   def self.find(id, db)
-      row = db.execute("SELECT * FROM pokemon WHERE id = ?;", id).flatten
-      Pokemon.new(id: row[0], name: row[1], type: row[2], db: db)
+     sql = "SELECT * FROM pokemon WHERE id = ?"
+     db.execute(sql, id).map do |row|
+       Pokemon.new(id: row[0], name: row[1], type: row[2], db: db)
+     end.first
   end
-
+  
 end
+
+
+# def self.save(name, type, db)
+#   db.execute("INSERT INTO pokemon (name, type) VALUES (?,?);", name, type)
+#   @id = db.execute('SELECT last_insert_rowid() FROM pokemon')[0][0]
+# end
+# 
+# def self.find(id, db)
+#     row = db.execute("SELECT * FROM pokemon WHERE id = ?;", id).flatten
+#     Pokemon.new(id: row[0], name: row[1], type: row[2], db: db)
+# end
 
   # def self.save(name, type, db)
   #   sql = <<-SQL
